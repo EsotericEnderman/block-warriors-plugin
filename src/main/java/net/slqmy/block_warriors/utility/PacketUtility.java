07b9c77332c464aaad1ee075863de9bab4547266
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-07-09 18:30:53 +0100

    Use reflection instead of `CraftBukkit` types

--- src/main/java/net/slqmy/block_warriors/utility/PacketUtility.java
+++ src/main/java/net/slqmy/block_warriors/utility/PacketUtility.java
@@ -1,16 +1,34 @@
 package net.slqmy.block_warriors.utility;
 
 import net.minecraft.network.protocol.game.ClientboundGameEventPacket;
-import net.minecraft.server.level.ServerPlayer;
-import org.bukkit.craftbukkit.v1_20_R1.entity.CraftPlayer;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
+
+import java.lang.reflect.Field;
+
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public final class PacketUtility {
 	public static void respawnPlayer(@NotNull final Player player) {
-		final ServerPlayer serverPlayer = ((CraftPlayer) player).getHandle();
+		Field connectionField;
 
-		serverPlayer.connection.send(
+		try {
+			connectionField = player.getClass().getDeclaredField("h");
+		} catch (NoSuchFieldException | SecurityException exception) {
+			exception.printStackTrace();
+			return;
+		}
+
+		ServerGamePacketListenerImpl connection;
+
+		try {
+			connection = (ServerGamePacketListenerImpl) connectionField.get(player);
+		} catch (IllegalArgumentException | IllegalAccessException exception) {
+			exception.printStackTrace();
+			return;
+		}
+
+		connection.send(
 						new ClientboundGameEventPacket(
 										ClientboundGameEventPacket.IMMEDIATE_RESPAWN,
 										1
