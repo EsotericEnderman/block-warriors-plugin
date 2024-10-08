2f0250de0c1c037964bd58a70299be986cd61b8c
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-10-08 15:34:20 +0100

    Update project information

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,10 +37,10 @@ fun pascalcase(kebabcaseString: String): String {
 
 description = "A collection of fun minigames."
 
-val mainProjectAuthor = "Slqmy"
+val mainProjectAuthor = "Esoteric Enderman"
 val projectAuthors = listOfNotNull(mainProjectAuthor)
 
-val topLevelDomain = "net"
+val topLevelDomain = "dev"
 
 group = topLevelDomain + groupStringSeparator + mainProjectAuthor.lowercase() + groupStringSeparator + snakecase(rootProject.name)
 version = "1.0.0-SNAPSHOT"
---
+++ src/main/java/dev/esoteric_enderman/block_warriors_plugin/types/Countdown.java
@@ -0,0 +1,48 @@
+package dev.esoteric_enderman.block_warriors_plugin.types;
+
+import org.bukkit.ChatColor;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.jetbrains.annotations.NotNull;
+
+import dev.esoteric_enderman.block_warriors_plugin.BlockWarriorsPlugin;
+import dev.esoteric_enderman.block_warriors_plugin.enums.GameState;
+import dev.esoteric_enderman.block_warriors_plugin.utility.ConfigurationUtility;
+
+public final class Countdown extends BukkitRunnable {
+	private final BlockWarriorsPlugin plugin;
+	private final Arena arena;
+
+	private int countdownSeconds;
+
+	public Countdown(@NotNull final BlockWarriorsPlugin plugin, @NotNull final Arena arena) {
+		this.plugin = plugin;
+		this.arena = arena;
+
+		this.countdownSeconds = ConfigurationUtility.getCountdownSeconds();
+	}
+
+	public void start() {
+		arena.setState(GameState.COUNTDOWN);
+
+		runTaskTimer(plugin, 0, 20);
+	}
+
+	@Override
+	public void run() {
+		if (countdownSeconds == 0) {
+			arena.start();
+
+			cancel();
+			return;
+		}
+
+		if (countdownSeconds <= 10 || countdownSeconds % 15 == 0) {
+			arena.sendTitle(ChatColor.YELLOW.toString() + countdownSeconds);
+			arena.sendMessage(
+							ChatColor.YELLOW + "The game will start in " + ChatColor.RED + countdownSeconds
+											+ ChatColor.YELLOW + " second" + (countdownSeconds == 1 ? "" : "s") + ".");
+		}
+
+		countdownSeconds--;
+	}
+}
