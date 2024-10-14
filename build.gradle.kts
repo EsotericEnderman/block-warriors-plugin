4a103977a1f5c4ae6870950490833746e451800f
Esoteric Enderman <esotericenderman@gmail.com>
2024-10-14 18:53:11 +0100

    Actually get the property

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,6 +80,6 @@ bukkitPluginYaml {
 
   version = project.version.toString()
   apiVersion = paperApiVersion
-  main = "${project.group}.minecraft.plugins.blockwarriors.${name}Plugin"
+  main = "${project.group}.minecraft.plugins.blockwarriors.${name.get()}Plugin"
   load = BukkitPluginYaml.PluginLoadOrder.POSTWORLD
 }
